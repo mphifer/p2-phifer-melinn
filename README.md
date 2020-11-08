179ae03674f367380a28105093458d2c30bab960
Melinn Phifer <mphifer@ufl.edu>
2020-11-08 12:46:23 -0500

    add accordion to forest page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # p2-phifer-melinn
+
+JS ACCORDION
+https://www.w3schools.com/howto/howto_js_accordion.asp
--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ onHove[0].addEventListener('mouseenter', openDesktop);
 onHove[0].addEventListener('mouseleave', openDesktop);
 
 
-//Read more
+//READ MORE BUTTONS
 var newPara = document.createElement("p")
 newPara.innerHTML = "We have new mulch near the front gate, next to the Wildflower Meadow, waterview butterfly garden, and at the end the Pine Forest.We will complete the mulch in the Wildflower Meadow. Finished the mulching of the Pine Forest. Weeded south of the waterview Butterfly Garden"
 document.getElementById("button1").addEventListener("click",
@@ -59,3 +59,19 @@ document.getElementById("button2").addEventListener("click",
     document.getElementById("open-two").appendChild(secondPara);
   }, false
 );
+
+//ACCORDION
+var accord = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accord.length; i++) {
+  accord[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
