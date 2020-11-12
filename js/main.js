2901ceb9f50832ff55912da5b4b5b7475d3a0bd1
Melinn Phifer <mphifer@ufl.edu>
2020-11-12 11:09:40 -0500

    change color of accordion + and - icons

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
 
-      //print weather at the end of main section text
+      //print weather at the end of the page before footer
         function showWeather() {
           var weatherText = document.createTextNode(apiResult.weather[0].description);
           weatherMessage.appendChild(weatherText);
@@ -17,7 +17,7 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=34285,us&appid=6efff70fe1477748e31c17d1c504635f', true);
 xmlhttp.send();
 
-//HOVER AND OPEN UP SUBMENU //
+//MOBILE - HOVER AND OPEN UP SUBMENU //
 var onHover = document.getElementsByClassName('main-menu-labels');
 var showMenu = document.getElementsByClassName('sub-menu');
 
@@ -28,7 +28,7 @@ function openMenu() {
 onHover[0].addEventListener('mouseenter', openMenu);
 onHover[0].addEventListener('mouseleave', openMenu);
 
-
+//DESKTOP - HOVER AND OPEN UP SUBMENU //
 var onHove = document.getElementsByClassName('desktop-labels');
 var showDesk = document.getElementsByClassName('nav-menu');
 
